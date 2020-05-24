b299d4b7be04a8361a08784d7fb071d08799022d
acheinu1 <acheinue@gmail.com>
2020-05-24 17:32:51 +0100

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,8 @@ var Name = document.querySelector(".name")
 var temp = document.querySelector(".temperature")
 
 button.addEventListener("click", function(){
-    fetch("https://api.openweathermap.org/data/2.5/weather?q="+inputval.value+",uk&appid=e09b18071bd4402245cfda328d9554da")
+// fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid=50a7aa80fa492fa92e874d23ad061374')
+fetch("https://api.openweathermap.org/data/2.5/weather?q="+inputval.value+"&appid=e09b18071bd4402245cfda328d9554da")
     .then(response=> response.json())
     .then(data => console.log(data))
 
